76258985ddcd5f0579f6e8f2aec108118768053c
Jerry Ng <ngshiheng@gmail.com>
2021-10-11 04:09:19 +0800

    Create a config package to get all config value

---
+++ config/config.go
@@ -0,0 +1,31 @@
+// In here, we have created Conf struct with embedded struct serverConf to get initial server configs.
+// So, when we need to add new configurations for DB in future,
+// we can easily add another embedded struct for them to maintain simplicity and readability of the code.
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/joeshaw/envdecode"
+)
+
+type Conf struct {
+	Server serverConf
+}
+
+type serverConf struct {
+	Port         int           `env:"SERVER_PORT,required"`
+	TimeoutRead  time.Duration `env:"SERVER_TIMEOUT_READ,required"`
+	TimeoutWrite time.Duration `env:"SERVER_TIMEOUT_WRITE,required"`
+	TimeoutIdle  time.Duration `env:"SERVER_TIMEOUT_IDLE,required"`
+}
+
+func AppConfig() *Conf {
+	var c Conf
+	if err := envdecode.StrictDecode(&c); err != nil {
+		log.Fatalf("Failed to decode: %s", err)
+	}
+
+	return &c
+}
