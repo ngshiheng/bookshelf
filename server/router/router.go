375fc8047d972dc454a1a6b11badee5c8df3d190
Jerry Ng <ngshiheng@gmail.com>
2021-10-11 08:06:34 +0800

    Add content type json middleware and API version in route

--- server/router/router.go
+++ server/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"bookshelf/server/app"
 	"bookshelf/server/requestlog"
+	"bookshelf/server/router/middleware"
 
 	"github.com/go-chi/chi"
 )
@@ -17,12 +18,17 @@ func New(a *app.App) *chi.Mux {
 	r.Method("GET", "/healthz/readiness", requestlog.NewHandler(a.HandleReady, l))
 	r.Method("GET", "/", requestlog.NewHandler(a.HandleIndex, l))
 
-	// Routes for books
-	r.Method("GET", "/books", requestlog.NewHandler(a.HandleListBooks, l))
-	r.Method("POST", "/books", requestlog.NewHandler(a.HandleCreateBook, l))
-	r.Method("GET", "/books/{id}", requestlog.NewHandler(a.HandleReadBook, l))
-	r.Method("PUT", "/books/{id}", requestlog.NewHandler(a.HandleUpdateBook, l))
-	r.Method("DELETE", "/books/{id}", requestlog.NewHandler(a.HandleDeleteBook, l))
+	// First import "bookshelf/app/router/middleware"
+	// Routes for APIs
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Use(middleware.ContentTypeJson)
+		// Routes for books
+		r.Method("GET", "/books", requestlog.NewHandler(a.HandleListBooks, l))
+		r.Method("POST", "/books", requestlog.NewHandler(a.HandleCreateBook, l))
+		r.Method("GET", "/books/{id}", requestlog.NewHandler(a.HandleReadBook, l))
+		r.Method("PUT", "/books/{id}", requestlog.NewHandler(a.HandleUpdateBook, l))
+		r.Method("DELETE", "/books/{id}", requestlog.NewHandler(a.HandleDeleteBook, l))
+	})
 
 	return r
 }
